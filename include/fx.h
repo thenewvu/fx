835776a86270af5a560e2ef159d3c7bc586d8f6b
Vu Le <thenewvu@gmail.com>
2018-03-25 13:36:01 +0700

    refactor

--- include/fx.h
+++ include/fx.h
@@ -5,43 +5,10 @@
 
 #include <stdio.h>
 
-#define FX_EACH(fx)                                                            \
-  Fixture fx = fixtures[0];                                                    \
-  for (int i = 0; i < sizeof(fixtures) / sizeof(fixtures[0]);                  \
-       ++i, fx = fixtures[i])
-
-#define FX_DATA(...) Fixture fixtures[] = __VA_ARGS__
-
-#define FX_TYPE(...) typedef struct __VA_ARGS__ Fixture
-
-#define FX_CASE(name, ...)                                                     \
-  do {                                                                         \
-    const char *__FX_CASE__ = name;                                            \
-    __VA_ARGS__                                                                \
-  } while (0)
-
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_YELLOW "\x1b[33m"
 #define ANSI_COLOR_RESET "\x1b[0m"
-
-#define FX_FAIL(...)                                                           \
-  do {                                                                         \
-    __FX_FAILED__++;                                                           \
-    printf(ANSI_COLOR_YELLOW);                                                 \
-    printf("FAILED | %s | ", __FX_CASE__);                                     \
-    printf(__VA_ARGS__);                                                       \
-    printf("\n");                                                              \
-    printf(ANSI_COLOR_RESET);                                                  \
-  } while (0)
-
-#define FX_PASS(...)                                                           \
-  do {                                                                         \
-    __FX_PASSED__++;                                                           \
-    printf("PASSED | %s | ", __FX_CASE__);                                     \
-    printf(__VA_ARGS__);                                                       \
-    printf("\n");                                                              \
-  } while (0)
 
 #define FX_MAIN(name, ...)                                                     \
   int main() {                                                                 \
@@ -61,3 +28,36 @@
       printf(ANSI_COLOR_RESET);                                                \
     }                                                                          \
   }
+
+#define FX_CASE(name, ...)                                                     \
+  do {                                                                         \
+    const char *__FX_CASE__ = name;                                            \
+    __VA_ARGS__                                                                \
+  } while (0)
+
+#define FX_TYPE(...) typedef struct __VA_ARGS__ Fixture
+
+#define FX_DATA(...) Fixture fixtures[] = __VA_ARGS__
+
+#define FX_EACH(fx)                                                            \
+  Fixture fx = fixtures[0];                                                    \
+  for (int i = 0; i < sizeof(fixtures) / sizeof(fixtures[0]);                  \
+       ++i, fx = fixtures[i])
+
+#define FX_FAIL(...)                                                           \
+  do {                                                                         \
+    __FX_FAILED__++;                                                           \
+    printf(ANSI_COLOR_YELLOW);                                                 \
+    printf("FAILED | %s | ", __FX_CASE__);                                     \
+    printf(__VA_ARGS__);                                                       \
+    printf("\n");                                                              \
+    printf(ANSI_COLOR_RESET);                                                  \
+  } while (0)
+
+#define FX_PASS(...)                                                           \
+  do {                                                                         \
+    __FX_PASSED__++;                                                           \
+    printf("PASSED | %s | ", __FX_CASE__);                                     \
+    printf(__VA_ARGS__);                                                       \
+    printf("\n");                                                              \
+  } while (0)
